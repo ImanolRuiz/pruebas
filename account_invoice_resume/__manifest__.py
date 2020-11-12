f4faf9c8245aac957850e907ef18e34d96aae663
ImanolRuiz <72189572+ImanolRuiz@users.noreply.github.com>
2020-11-12 10:32:37 +0100

    Add files via upload

---
+++ account_invoice_resume/__manifest__.py
@@ -0,0 +1,45 @@
+###############################################################################
+#
+#    SDi Soluciones Informáticas
+#    Copyright (C) 2020-Today SDi Soluciones Informáticas <www.sdi.es>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+{
+    'name': "account_invoice_resume",
+
+    'summary': """
+        Crea un formato de factura resumido""",
+
+
+    'author': "SDi Soluciones Informáticas",
+    'website': "https://www.sdi.es/odoo",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['account'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'report/account_invoice_report.xml',
+        'report/account_invoice_templates.xml',
+    ],
+}
